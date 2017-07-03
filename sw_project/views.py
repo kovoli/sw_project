15eb50ec0d38402e0e1ea4c28bb33487a0cd78e5
kovoli <kovoli1985@gmail.com>
2017-07-03 21:57:37 +0200

    Блог финиш, магазины финиш, !!! deals начать делать

--- sw_project/views.py
+++ sw_project/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from stores.models import Store
-
+from blog.models import Post
 
 def lists(request):
     stores = Store.objects.all().order_by('title')
+    posts = Post.objects.all().order_by('-publish')[:3]
 
-    return render(request, 'content.html', {'stores': stores})
+    return render(request, 'content.html', {'stores': stores, 'posts': posts})
